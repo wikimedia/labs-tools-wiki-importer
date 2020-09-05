8edf0670b01157029dcf412b6bd2bbfa9f470b72
Martin Urbanec <martin.urbanec@wikimedia.cz>
2020-09-05 19:45:49 +0200

    Background mw requests
    Change-Id: I890f13a2364a01ec07711fda3e149610250325c6

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@
 import os
 import yaml
 from flask import redirect, request, jsonify, render_template, url_for, \
-    make_response, flash
+    make_response, flash, session
 from flask import Flask
 import requests
 import subprocess
@@ -26,6 +26,7 @@ from flask_mwoauth import MWOAuth
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from celery import Celery
+from requests_oauthlib import OAuth1
 
 app = Flask(__name__, static_folder='../static')
 
@@ -33,6 +34,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 ALLOWED_GROUPS = ['new-wikis-importer', 'steward']
+useragent = 'WikiImporter (tools.wiki-importer@tools.wmflabs.org)'
 
 # Load configuration from YAML file
 __dir__ = os.path.dirname(__file__)
@@ -74,6 +76,13 @@ def make_celery():
     return celery
 
 celery = make_celery()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
+    token_key = db.Column(db.String(255))
+    token_secret = db.Column(db.String(255))
 
 class Wiki(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -163,6 +172,27 @@ class Wiki(db.Model):
 def logged():
     return mwoauth.get_current_user() is not None
 
+def get_user():
+    return User.query.filter_by(
+        username=mwoauth.get_current_user()
+    ).first()
+
+def mw_request(data, url=None, user=None):
+    if url is None:
+        api_url = mwoauth.api_url + "/api.php"
+    else:
+        api_url = url
+    if user is None:
+        access_token = session.get('mwoauth_access_token', {})
+        request_token_secret = access_token.get('secret').decode('utf-8')
+        request_token_key = access_token.get('key').decode('utf-8')
+    else:
+        request_token_secret = user.token_secret
+        request_token_key = user.token_key
+    auth = OAuth1(app.config.get('CONSUMER_KEY'), app.config.get('CONSUMER_SECRET'), request_token_key, request_token_secret)
+    data['format'] = 'json'
+    return requests.post('https://meta.wikimedia.org/w/api.php', data=data, auth=auth, headers={'User-Agent': useragent})
+
 @app.context_processor
 def inject_base_variables():
     return {
@@ -175,6 +205,29 @@ def ensure_login():
     if request.path != '/login' and request.path != '/oauth-callback':
         if not logged():
             return render_template('login.html')
+
+@app.before_request
+def db_init_user():
+    if logged():
+        user = get_user()
+        access_token = session.get('mwoauth_access_token', {})
+        request_token_secret = access_token.get('secret').decode('utf-8')
+        request_token_key = access_token.get('key').decode('utf-8')
+        if user is None:
+            user = User(
+                username=mwoauth.get_current_user(),
+                token_key=request_token_key,
+                token_secret=request_token_key,
+            )
+            db.session.add(user)
+            db.session.commit()
+        else:
+            user.token_key = request_token_key
+            user.token_secret = request_token_secret
+            if not user.is_active:
+                return render_template('permission_denied.html'), 403
+            
+            db.session.commit()
 
 @app.before_request
 def ensure_privileges():
@@ -257,6 +310,13 @@ def wiki_split(dbname):
 def wiki_import(dbname):
     wiki = Wiki.query.filter_by(dbname=dbname)[0]
     return render_template('wiki_import.html', wiki=wiki)
+
+@app.route('/test.json')
+def test():
+    return jsonify(mw_request({
+        'action': 'query',
+        'meta': 'globaluserinfo'
+    }).json())
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
